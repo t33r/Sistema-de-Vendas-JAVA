b7c8512d15c60cb11c4f116a7478c68d5d6f2a2a
Tauã Bernardo <tbptffc@gmail.com>
2024-06-17 20:16:09 -0300

    Implementação do formulário de login, usuário e principal

---
+++ src/classes/Dados.java
@@ -0,0 +1,12 @@
+
+package classes;
+
+
+public class Dados {
+    public boolean validarUsuario(String usuario, String senha) {
+        if (usuario.equals("san1") && senha.equals("123")) {
+        return true;
+    } else 
+            return false;
+    }
+}
---
+++ src/classes/JavaVenda.java
@@ -0,0 +1,16 @@
+
+package classes;
+
+import formularios.FrmLogin;
+
+
+public class JavaVenda {
+
+
+    public static void main(String[] args) {
+        FrmLogin miLogin = new FrmLogin();
+        miLogin.setLocationRelativeTo(null);
+        miLogin.setVisible(true);
+    }
+    
+}
